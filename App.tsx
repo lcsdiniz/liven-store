ccb13004c80cf4d763c4eaae8d6765fb841df1a7
Lucas Diniz <lucasdiniz.smt@gmail.com>
2023-10-22 12:35:53 -0300

    feat: status bar dark

--- App.tsx
+++ App.tsx
@@ -2,11 +2,16 @@ import { ThemeProvider } from 'styled-components/native';
 import theme from './src/theme/theme';
 import Stack from './src/navigation/Stack';
 import { CartProvider } from './src/contexts/CartContext';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <CartProvider>
+        <StatusBar
+          animated={true}
+          barStyle="dark-content"
+        />
         <Stack />
       </CartProvider>
     </ThemeProvider>
