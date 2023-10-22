c64a1188fa5add0da01226436a70b18774b3d07f
Lucas Diniz <lucasdiniz.smt@gmail.com>
2023-10-22 12:07:48 -0300

    feat: Cart

---
+++ src/contexts/CartContext.tsx
@@ -0,0 +1,66 @@
+import React, { createContext, ReactNode } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Product } from '../types/product';
+
+type CartProduct = Product & {
+	quantity: number
+}
+
+type Cart = CartProduct[]
+
+export interface CartContextType {
+  getCart: (email: string, password: string) => Promise<Cart>;
+  updateCart: (product: Product) => Promise<void>;
+  deleteCart: (email: string, password: string) => Promise<void>;
+}
+
+export const CartContext = createContext<CartContextType | undefined>(undefined);
+
+interface CartProviderProps {
+  children: ReactNode;
+}
+
+export const CartProvider: React.FC<CartProviderProps> = ({ children }) => {
+  async function getCart() {
+	const cart = await AsyncStorage.getItem('@liven-cart')
+
+	if(cart) {
+		const parsedCart: Cart = JSON.parse(cart)
+		return parsedCart
+	}
+
+	return []
+  };
+
+  async function updateCart(product: Product) {
+	const cart = await AsyncStorage.getItem('@liven-cart')
+
+	if(cart) {
+		const parsedCart: Cart = JSON.parse(cart)
+		const alreadyInCart = parsedCart.find(product => product.id)
+
+		if(alreadyInCart) {
+			const productIndex = parsedCart.findIndex(product => product.id)
+			parsedCart[productIndex].quantity = parsedCart[productIndex].quantity + 1
+		} else {
+			parsedCart.push({ ...product, quantity: 1 })
+		}
+
+		await AsyncStorage.setItem('@liven-cart', JSON.stringify(parsedCart))
+	} else if(!cart) {
+		await AsyncStorage.setItem('@liven-cart', JSON.stringify([
+			{...product, quantity: 1 }
+		]))
+	}
+  };
+
+  async function deleteCart() {
+    await AsyncStorage.removeItem('@liven-cart');
+  };
+
+  return (
+    <CartContext.Provider value={{ getCart, updateCart, deleteCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
